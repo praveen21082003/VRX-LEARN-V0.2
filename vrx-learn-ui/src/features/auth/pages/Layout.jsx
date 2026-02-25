de9df81dc20b2e761b8f427202887d3114d0b7a1
LilithVishwa G <lilith04062005@gmail.com>
2026-02-25 12:59:03 +0530

    Modified Login and Layout

--- vrx-learn-ui/src/features/auth/pages/Layout.jsx
+++ vrx-learn-ui/src/features/auth/pages/Layout.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
-import Login from './Login'
+import Login from "./Login";
 
 function Layout() {
   return (
-    <div className="flex h-screen w-full justify-center items-center bg-surface">
-      <div className="flex w-full max-w-4xl h-[85vh] rounded-xl overflow-hidden shadow-lg bg-white">
-       <div className="md:hidden lg:block relative w-1/2 bg-primary overflow-hidden">
+    // 'bg-surface' for desktop, 'bg-white' for mobile 
+    <div className="flex min-h-screen w-full justify-center items-center bg-[#ffffff] md:bg-surface p-4 lg:p-5 xl:p-5 ">
+      {/* Container: Full width on mobile, max-w-4xl on desktop */}
 
+      <div
+        className="flex w-full h-full md:h-[95vh] 
+                md:max-w-3xl lg:max-w-4xl xl:max-w-7xl 
+                2xl:max-w-none 2xl:w-[85%] 2xl:p-0
+                rounded-xl overflow-hidden md:shadow-lg bg-white"
+      >
+        {/* LEFT SECTION: Hidden on mobile (hidden), shown on medium screens and up (md:flex) */}
+        <div className="hidden md:block relative w-1/2 bg-primary overflow-hidden">
           <img
-            src='/src/assets/images/Learning-Illustration.png'
+            src="/src/assets/images/Learning-Illustration.png"
             alt="Background Decor"
-            className="absolute top-35 left-[-11%] w-4/5 opacity-16 scale-110 pointer-events-none"
+            className="absolute top-35 sm:top-55 lg:top-45 xl:top-35  left-[-11%] w-4/5  opacity-16 scale-110 pointer-events-none  "
           />
-
           <img
-            src='/src/assets/images/Learning-Illustration.png'
+            src="/src/assets/images/Learning-Illustration.png"
             alt="Login Illustration"
-            className="absolute bottom-7 right-1 w-8px h-20px scale-105 object-contain z-10"
+            className="absolute bottom-7 right-1 w-8px h-20px scale-105  object-contain z-10"
           />
-          
         </div>
 
-        <div className="w-1/2 flex flex-col items-center justify-center">
+        {/* RIGHT SECTION (The Form): Full width on mobile, half width on desktop */}
+        <div className="w-full md:w-1/2 flex flex-col items-center justify-center bg-white py-10">
           <Login />
         </div>
       </div>
     </div>
-
-  )
+  );
 }
 
-export default Layout
+export default Layout;
--- vrx-learn-ui/src/features/auth/pages/Login.jsx
+++ vrx-learn-ui/src/features/auth/pages/Login.jsx
@@ -1,112 +1,136 @@
-import React, { useState } from 'react'
-import { Icon, Input, Button } from "@/components/ui"
-import { useAuth } from '@/context/AuthContext';
-import { getUserProfile } from '@/services/profile.service'
-import { replace, useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { Icon, Input, Button } from "@/components/ui";
+import { useAuth } from "@/context/AuthContext";
+import { getUserProfile } from "@/services/profile.service";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+  const { setUser, setLoading } = useAuth();
+  const [warning, setWarning] = useState({ email: "", password: "" });
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
 
-    const navigate = useNavigate();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials((prev) => ({ ...prev, [name]: value }));
+  };
 
-    const { setUser, setLoading } = useAuth();
-    const [warning, setWarning] = useState({
-        email: "",
-        password: ""
-    })
+  async function fetchProfile() {
+    try {
+      setLoading(true);
+      const data = await getUserProfile(
+        credentials.email,
+        credentials.password,
+      );
+      setUser(data[0]);
 
-    const [credentials, setCredentials] = useState({
-        email: "",
-        password: ""
-    });
+      if (data[0].role === "TRAINEE") {
+        navigate("/dashboard", { replace: true });
+      } else {
+        navigate("/admin", { replace: true });
+      }
+    } catch (err) {
+      console.error("Profile fetch failed", err);
+    } finally {
+      setLoading(false);
+    }
+  }
 
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-
-        setCredentials((prev) => ({
-            ...prev,
-            [name]: value
-        }));
-    };
-
-
-    async function fetchProfile() {
-        try {
-            setLoading(true);
-            const data = await getUserProfile(credentials.email, credentials.password);
-            setUser(data[0]);
-
-            if (data[0].role === "TRAINEE") {
-                navigate("/dashboard", { replace: true });
-            }
-            else {
-                navigate("/dashboard", { replace: true })
-            }
-
-        } catch (err) {
-            console.error("Profile fetch failed", err);
-        } finally {
-            setLoading(false);
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!credentials.email) {
+      setWarning((prev) => ({ ...prev, email: "Please provide email" }));
+    }
+    if (!credentials.password) {
+      setWarning((prev) => ({ ...prev, password: "Password can't be empty" }));
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    fetchProfile();
+  };
 
-        if (!credentials.email) {
-            setWarning(prev => ({ ...prev, email: "Please provide email" }))
-        }
-        if (!credentials.password) {
-            setWarning(prev => ({ ...prev, password: "Password can't be empty" }))
-        }
-        fetchProfile();
-    };
+  return (
+    <div className="flex flex-col items-center w-full max-w-md xl:max-w-lg  px-8 ">
+      {/* Header Section */}
+      <div className="flex flex-col items-center gap-3  mb-8 ">
+        <img
+          src="/VRX-logo.svg"
+          alt="VRX Logo"
+          className="h-24 w-24 object-contain xl:h-28 xl:w-28  "
+        />
+        <h1 className="text-2xl sm:text-3xl  lg:text-4xl font-semibold text-gray-800  ">
+          Welcome Back!
+        </h1>
+      </div>
 
+      {/* Form Section */}
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 w-full xl:gap-6 2xl:gap-8 "
+    
+      >
+        <Input
+          name="email"
+          label="Email" 
+          placeholder="Enter your email id"
+          icon={<Icon name="ic:outline-email" width="18px" height="18px" />}
+          inputWarning={warning.email}
+          onChange={handleChange}
+          value={credentials.email}
+        />
 
-    return (
-        <>
-            <div className="flex flex-col items-center gap-2">
-                <img
-                    src="/VRX-logo.svg"
-                    alt="VRX Logo"
-                    className="h-16 w-16 object-contain"
-                />
+        <div className="flex flex-col gap-1">
+          <Input
+            name="password"
+            type="password"
+            label="Password"
+            placeholder="Enter your password"
+            icon={
+              <Icon name="material-symbols:lock" width="18px" height="18px" />
+            }
+            inputWarning={warning.password}
+            onChange={handleChange}
+            value={credentials.password}
+          />
+          {/* Forgot Password Link from L2 */}
+          <a href="#" className="text-xs text-gray-500 hover:underline mt-1">
+            Forgot Password?
+          </a>
+        </div>
 
-                <h1 className="text-2xl font-semibold text-gray-900">
-                    Welcome Back!
-                </h1>
+        <Button
+          type="submit"
+          className="p-4 mt-4 rounded-lg font-bold text-white bg-primary"
+          buttonName="Login"
+        />
+      </form>
 
-                <p className="text-sm text-gray-500">
-                    Please login to continue
-                </p>
-            </div>
-            <form onSubmit={handleSubmit} className="flex flex-col gap-5 w-[80%]">
-                <Input
-                    name="email"
-                    label="Email"
-                    placeholder="enter your email ID"
-                    icon={<Icon name="ic:outline-email" width="18px" height="18px" />}
-                    inputWarning={warning.email}
-                    onChange={handleChange}
-                    value={credentials.email}
-                />
-
-                <Input
-                    name="password"
-                    type="password"
-                    label="Password"
-                    placeholder="enter your password"
-                    icon={<Icon name="material-symbols:lock" width="18px" height="18px" />}
-                    inputWarning={warning.password}
-                    onChange={handleChange}
-                    value={credentials.password}
-                />
-
-                <Button type="submit" className="p-3 rounded-lg font-semibold text-sm" buttonName="Login" buttonColor="primary" />
-            </form>
-
-        </>
-    )
+      {/* Social Links Section from L2 */}
+      <div className="mt-12 flex flex-col items-center gap-4">
+        <p className="text-xs text-gray-500 lg:text-sm xl:text-lg">
+          Connect with us
+        </p>
+        <div className="flex gap-4 ">
+          {/* <Icon name="mdi:web" className="text-blue-500 text-2xl " /> */}
+          <Icon
+            name="streamline-plump-color:web"
+            className="text-blue-500 text-xl h-8 w-8"
+          />
+          <Icon
+            name="mdi:github"
+            className="text-black text-2xl h-8 w-8"
+          />
+          <Icon
+            name="skill-icons:linkedin"
+            className="text-blue-700 text-2xl h-8 w-8"
+          />
+          <Icon
+            name="logos:youtube-icon"
+            className="text-red-600 text-2xl h-8 w-8"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
